3df31357a47c4e15acb5ba98576f08d52e4524d0
Suge <suge.david@healthcloud.co.ke>
2022-08-10 00:38:27 +0300

    feat: google maps

---
+++ lib/domain/value_objects/app_strings.dart
@@ -0,0 +1,18 @@
+// login page
+const String loginText = 'Log In';
+const String loginTitle = 'Welcome back to Sendy';
+const String loginBody = 'Please fill in the details below to log in.';
+const String emailText = 'Email';
+const String passwordText = 'Password';
+const String forgotPasswordText = 'FORGOT PASSWORD';
+
+// home page
+const String enterPickupText = 'Enter Pickup';
+const String shareText = 'Share Invites';
+const String profileText = 'Profile';
+const String paymentText = 'Payment';
+const String orderHistoryText = 'Order History';
+const String promotionsText = 'Promotions';
+const String helpText = 'Help';
+const String appVersionText = 'Sendy v 0.0.1';
+const String userText = 'Suge David';
--- lib/presentation/LogInPage.dart
+++ lib/presentation/LogInPage.dart
@@ -1,42 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:sendy_case_study/shared/Colors.dart';
 
-class LoginPage extends StatelessWidget {
+import '../domain/value_objects/app_strings.dart';
+import '../shared/components/Login/LoginForm.dart';
+
+class LoginPage extends StatefulWidget {
   const LoginPage();
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      body: const Center(
-        child: SizedBox(
-          width: 400,
-          child: Card(
-            child: SignUpForm(),
-          ),
-        ),
-      ),
-    );
-  }
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class SignUpForm extends StatefulWidget {
-  const SignUpForm();
-
-  @override
-  _SignUpFormState createState() => _SignUpFormState();
-}
-
-class _SignUpFormState extends State<SignUpForm> {
-  final _emailTextController = TextEditingController();
-  final _passwordTextController = TextEditingController();
-
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         title: Text(
-          'Log In',
+          loginText,
           style: TextStyle(
             color: Colors.white,
             fontSize: 16,
@@ -46,94 +30,18 @@ class _SignUpFormState extends State<SignUpForm> {
       ),
       body: Form(
         child: Padding(
-          padding: const EdgeInsets.all(28.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              // LinearProgressIndicator(value: _formProgress),
-              SizedBox(
-                height: 15,
-              ),
-              Text(
-                'Welcome back to Sendy',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Please fill in the details below to log in.',
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-
-              // Email
-              TextFormField(
-                controller: _emailTextController,
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-
-              // Password
-              TextFormField(
-                controller: _passwordTextController,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-
-              // Log In
-              Container(
-                width: double.infinity,
-                height: 45,
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    primary: Colors.white,
-                    backgroundColor: AppColors.primaryColor,
-                    shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(50))),
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    'LOG IN',
-                    style: TextStyle(
-                      fontSize: 15,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-
-              // Forgot Password
-              Center(
-                child: Text(
-                  'FORGOT PASSWORD',
-                  style: TextStyle(
-                    color: AppColors.primaryColor,
-                    fontSize: 15,
-                  ),
-                ),
-              ),
-            ],
-          ),
+          padding: EdgeInsets.all(28.0),
+          child: width >= 500
+              ? Center(
+                  child: SizedBox(
+                      width: 400,
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(38.0),
+                          child: LoginForm(),
+                        ),
+                      )))
+              : LoginForm(),
         ),
       ),
     );
